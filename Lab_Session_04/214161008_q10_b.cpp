ee672032c1f31e5fa4cf2cf0f755477a1779a485
k_rajendra <31775586+rajendrakujur@users.noreply.github.com>
2021-10-08 20:56:51 +0530

    Update 214161008_q10_b.cpp

--- Lab_Session_04/214161008_q10_b.cpp
+++ Lab_Session_04/214161008_q10_b.cpp
@@ -1,19 +1,21 @@
-/*This program is last updated by RAJENDRA KUJUR (214161008) at 18/08/2021 19:20*/
+/*This program is last updated by RAJENDRA KUJUR (214161008) at 13-09-2021 19:20*/
 
 #include <iostream>
 #include <limits.h>
-#include <unistd.h>
+#include <fstream>
 using namespace std;
 
 // structure definition of process
 struct Process
 {
+    int process_id;
     int arrival_time;
     int burst_time;
     int start_time;
     int end_time;
     int waiting_time;
     int turn_around_time;
+    int response_time;
     bool executed;
 };
 
@@ -36,16 +38,31 @@ void makeCopy(struct Process p[], struct Process copy[], int number_of_process)
 {
     for (int index = 0; index < number_of_process; index++)
     {
+        copy[index].process_id = p[index].process_id;
         copy[index].arrival_time = p[index].arrival_time;
         copy[index].burst_time = p[index].burst_time;
         copy[index].executed = p[index].executed;
     }
 }
 
+// returns the first process starting time
+int minimum_starting_time(struct Process p[], int number_of_process)
+{
+    int min_time = INT_MAX;
+    for (int index = 0; index < number_of_process; index++)
+    {
+        if (p[index].arrival_time < min_time)
+        {
+            min_time = p[index].arrival_time;
+        }
+    }
+    return min_time;
+}
+
 // calculates the total burst time of all the processes and returns
 int totalTime(struct Process p[], int number_of_process)
 {
-    int total_time = 0;
+    int total_time = minimum_starting_time(p, number_of_process);
     for (int index = 0; index < number_of_process; index++)
     {
         total_time += p[index].burst_time;
@@ -54,17 +71,23 @@ int totalTime(struct Process p[], int number_of_process)
 }
 
 // returns the process number having minimum burst time at a time
-int findMin(struct Process p[], int number_of_process, int time_counter)
+int findMin(struct Process copy[], int number_of_process, int time_counter)
 {
     int minimum_time = INT_MAX;
-    int minimum_index = 0;
+    int minimum_index = -1;
 
     for (int index = 0; index < number_of_process; index++)
     {
-        if (p[index].burst_time > 0 && p[index].burst_time < minimum_time && p[index].arrival_time <= time_counter)
+        if (copy[index].burst_time != 0)
         {
-            minimum_index = index;
-            minimum_time = p[index].burst_time;
+            if (copy[index].burst_time < minimum_time)
+            {
+                if (copy[index].arrival_time <= time_counter)
+                {
+                    minimum_index = index;
+                    minimum_time = copy[index].burst_time;
+                }
+            }
         }
     }
     return minimum_index;
@@ -82,70 +105,121 @@ void SRTF(struct Process p[], struct Process copy[], int number_of_process)
     // travel by one unit of time in each iteration
     for (int time_elapsed = 0; time_elapsed < total_time; time_elapsed++)
     {
-        min_index = findMin(p, number_of_process, time_elapsed);
-
+        min_index = findMin(copy, number_of_process, time_elapsed);
+        if (min_index == -1)
+        {
+            continue;
+        }
         // updates the burst time of minindex process
-        p[min_index].burst_time -= 1;
+        copy[min_index].burst_time -= 1;
 
         // if the process is executed first time then update its start time and change its executed flag to true
-        if (p[min_index].executed == false)
+        if (copy[min_index].executed == false)
         {
-            p[min_index].start_time = time_elapsed;
-            p[min_index].executed = true;
+            copy[min_index].start_time = time_elapsed;
+            copy[min_index].executed = true;
+            copy[min_index].response_time = copy[min_index].start_time - copy[min_index].arrival_time;
         }
 
         // if the process completed its execution then update its end time and therefore calculate its waiting time and turn around time
-        if (p[min_index].burst_time == 0)
+        if (copy[min_index].burst_time == 0)
         {
-            p[min_index].end_time = time_elapsed + 1;
-            p[min_index].turn_around_time = p[min_index].end_time - p[min_index].arrival_time;
-            p[min_index].waiting_time = p[min_index].turn_around_time - copy[min_index].burst_time;
+            copy[min_index].end_time = time_elapsed + 1;
+            copy[min_index].turn_around_time = copy[min_index].end_time - copy[min_index].arrival_time;
+            copy[min_index].waiting_time = copy[min_index].turn_around_time - p[min_index].burst_time;
         }
     }
 }
 
-// prints the final result in table format
-void printResult(struct Process p[], struct Process copy[], int number_of_process)
+// takes a string convert it into integer and returns
+int stringToInteger(string str)
 {
+    int str_index = 0;
+    int return_value = str[str_index++] - '0';
+
+    while (str[str_index] != '\0')
+    {
+        return_value *= 10;
+        return_value += str[str_index++] - '0';
+    }
+    return return_value;
+}
+
+// prints the final result in table format
+void writeToFile(struct Process p[], struct Process copyProcess[], int number_of_process)
+{
+    ofstream fout;
+    fout.open("214161008_q10_b_output.txt");
+
     // to store total waiting time and total turn around time
     int total_waiting_time = 0;
     int total_turn_around_time = 0;
 
-    cout << "\n\nProcess \tArrival Time\tBurst Time\tStarting Time\tFinishing Time\tTurn Around Time\tWaiting Time\n";
-    cout << "----------------------------------------------------------------------------------------------------------------\n";
-    for (int index = 0; index < number_of_process; index++)
+    fout << "PID\t\tA.T.\tB.T.\tS.T.\tE.T.\tT.A.T.\tW.T.\tR.T.\n";
+    fout << "______________________________________________________________________________________________________________\n";
+    for (int p_index = 0; p_index < number_of_process; p_index++)
     {
-        cout << "P" << (index + 1) << "\t\t" << p[index].arrival_time << "\t\t" << copy[index].burst_time << "\t\t" << p[index].start_time << "\t\t" << p[index].end_time << "\t\t" << p[index].turn_around_time << "\t\t" << p[index].waiting_time;
-        cout << endl;
+        for (int index = 0; index < number_of_process; index++)
+        {
+            if (copyProcess[index].process_id == p[p_index].process_id)
+            {
+                fout << p[p_index].process_id << "\t\t" << p[p_index].arrival_time << "\t\t" << p[p_index].burst_time << "\t\t" << copyProcess[index].start_time << "\t\t" << copyProcess[index].end_time << "\t\t" << copyProcess[index].turn_around_time << "\t\t" << copyProcess[index].waiting_time << "\t\t" << copyProcess[index].response_time << endl;
 
-        // calculate the total_waiting_time for all the processes
-        total_waiting_time += p[index].waiting_time;
+                // calculate the total_waiting_time for all the processes
+                total_waiting_time += copyProcess[index].waiting_time;
 
-        // calculate the total_turn_around_time for all the processes
-        total_turn_around_time += p[index].turn_around_time;
+                // calculate the total_turn_around_time for all the processes
+                total_turn_around_time += copyProcess[index].turn_around_time;
+            }
+        }
     }
 
     float avg_waiting_time = float(total_waiting_time) / float(number_of_process);
     float avg_turn_around_time = float(total_turn_around_time) / float(number_of_process);
 
-    cout << "\nAverage Waiting Time : " << avg_waiting_time;
-    cout << "\nAverage Turn Arount Time : " << avg_turn_around_time;
+    fout << "\nAverage Turn Arount Time : " << avg_turn_around_time;
+    fout << "\nAverage Waiting Time : " << avg_waiting_time;
+    fout << "\n\nPID : Process ID";
+    fout << "\nA.T. : Arrival Time";
+    fout << "\nB.T. : Burst Time";
+    fout << "\nS.T. : Starting Time";
+    fout << "\nE.T. : End Time";
+    fout << "\nT.A.T : Turn Around Time";
+    fout << "\nW.T. : Waiting Time";
+    fout << "\nR.T. : Response Time";
+    fout.close();
+    cout << "\nWritten to file '214161008_q10_b_output.txt' ";
 }
 
-// main function execution begins here
-int main()
+// reads the file and call required functions
+void readFromFile()
 {
-    // prompts the user for the number of the processes
-    int number_of_process;
-    cout << "Enter number of processes : ";
-    cin >> number_of_process;
+    ifstream fin;
+    fin.open("214161008_q10_b_input.txt");
 
-    // declare a structure and its copy (to print the result in well formatted manner)
-    struct Process p[number_of_process];
+    string line;
+
+    getline(fin, line);
+    getline(fin, line);
+    // to store all the process details
+    struct Process p[10000];
+    int number_of_process = 0;
+
+    // read til we reach to the end of the file
+    while (!fin.eof())
+    {
+        fin >> line;
+        p[number_of_process].process_id = stringToInteger(line);
+        fin >> line;
+        p[number_of_process].arrival_time = stringToInteger(line);
+        fin >> line;
+        p[number_of_process].burst_time = stringToInteger(line);
+        number_of_process++;
+    }
+    fin.close();
+    cout << "\nRead from file '214161008_q10_b_input.txt' ";
+
     struct Process copy[number_of_process];
-
-    // take input for all the processes
-    takeInput(p, number_of_process);
 
     // copy the contents into copy structure
     makeCopy(p, copy, number_of_process);
@@ -154,7 +228,13 @@ int main()
     SRTF(p, copy, number_of_process);
 
     // print the final result
-    printResult(p, copy, number_of_process);
+    writeToFile(p, copy, number_of_process);
+}
+
+// main function execution begins here
+int main()
+{
+    readFromFile();
     cout << endl;
 
     return 0;
